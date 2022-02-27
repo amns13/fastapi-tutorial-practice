e0c9a131c90c3634661c070698a9c7112e21226b
amns13 <slimmsady@gmail.com>
2022-02-27 15:07:29 +0530

    Tutorial part 5

--- main.py
+++ main.py
@@ -1,17 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/items1/")
+async def read_items(q: str | None = Query(None, min_length=3, max_length=50)):
+    """q is Optional"""
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items2/")
+async def read_items(q: str = Query("fixedquery", min_length=3)):
+    """q has default value "fixedquery"""
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items3/")
+async def read_items(q: str = Query(..., min_length=3)):
+    """q is mandatory with no default value"""
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items4/")
+async def read_items(q: list[str] = Query(["foo", "bar"])):
+    """q is a list with ['foo', 'bar'] as default value"""
+    query_items = {"q": q}
+    return query_items
+
+@app.get("/items5/")
+async def read_items(
+    q: str
+    | None = Query(
+        None,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+    )
+):
+    """Additiona; metadata with q"""
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
 
-# A different app. Both cam run in parallel.
-altapp = FastAPI()
-
-
-@altapp.get("/")
-async def root():
-    return {"message": "Hello World from alt"}
+@app.get("/items6/")
+async def read_items(q: str | None = Query(None, alias="item-query")):
+    """q is sent as ?item-query in url"""
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
