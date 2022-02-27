b8ce0066e276655d935a853d719bf81fbd5a571c
amns13 <slimmsady@gmail.com>
2022-02-27 21:38:20 +0530

    part 7 tutorial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Body
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -11,7 +11,18 @@ class Item(BaseModel):
     tax: float | None = None
 
 
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
 @app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results
+
+
+@app.put("/items1/{item_id}")
 async def update_item(
     *,
     item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000),
@@ -23,4 +34,34 @@ async def update_item(
         results.update({"q": q})
     if item:
         results.update({"item": item})
+    return results
+
+
+@app.put("/items2/{item_id}")
+async def update_item(
+    item_id: int, item: Item, user: User, importance: int = Body(...)
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    return results
+
+
+@app.put("/items3/{item_id}")
+async def update_item(
+    *,
+    item_id: int,
+    item: Item,
+    user: User,
+    importance: int = Body(..., gt=0),
+    q: str | None = None
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.put("/items4/{item_id}")
+async def update_item(item_id: int, item: Item = Body(..., embed=True)):
+    """In thois case key "item" is expected in request body"""
+    results = {"item_id": item_id, "item": item}
     return results
