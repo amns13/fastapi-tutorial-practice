30b3acb5c2ede62c792a053ab6826f2d939ea3c2
amns13 <slimmsady@gmail.com>
2022-02-27 14:30:41 +0530

    part 4

--- main.py
+++ main.py
@@ -2,16 +2,37 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-# A different app. Both cam run in parallel.
-altapp = FastAPI()
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 
-@altapp.get("/")
-async def root():
-    return {"message": "Hello World from alt"}
+@app.get("/items/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    """
+    item_id: Mandatory path parameter
+    needy: mandatory query parameter
+    skip: query parameter with default value 0
+    limit: Optional query param
+    """
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
