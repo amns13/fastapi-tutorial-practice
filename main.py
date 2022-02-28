8b07f67823d64509e5bac62811911c5debd64634
amns13 <slimmsady@gmail.com>
2022-02-28 22:58:02 +0530

    part 20

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-from fastapi import FastAPI
-
-app = FastAPI()
+from fastapi import Depends, FastAPI, Header, HTTPException
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def verify_token(x_token: str = Header(...)):
+    if x_token != "fake-super-secret-token":
+        raise HTTPException(status_code=400, detail="X-Token header invalid")
 
 
-# A different app. Both cam run in parallel.
-altapp = FastAPI()
+async def verify_key(x_key: str = Header(...)):
+    if x_key != "fake-super-secret-key":
+        raise HTTPException(status_code=400, detail="X-Key header invalid")
+    return x_key
 
 
-@altapp.get("/")
-async def root():
-    return {"message": "Hello World from alt"}
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+
+@app.get("/items/")
+async def read_items():
+    return [{"item": "Portal Gun"}, {"item": "Plumbus"}]
+
+
+@app.get("/users/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
