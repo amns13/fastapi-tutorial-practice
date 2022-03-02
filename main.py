fa769723e6733a124c6ec51d563b153af52e3f4a
amns13 <slimmsady@gmail.com>
2022-03-02 22:49:29 +0530

    part 23

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
+# A different app. Both cam run in parallel.
+altapp = FastAPI()
 
 
-def fake_decode_token(token):
-    return User(
-        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
-    )
-
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
-    return user
-
-
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
+@altapp.get("/")
+async def root():
+    return {"message": "Hello World from alt"}
